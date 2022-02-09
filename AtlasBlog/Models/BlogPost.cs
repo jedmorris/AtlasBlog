081c15b9fab8effefa520abc21d8d52905f2ebc5
jedmorris <jemorris110@gmail.com>
2022-02-09 08:37:51 -0800

    updated blogPostState to include html@enum listing

--- AtlasBlog/Models/BlogPost.cs
+++ AtlasBlog/Models/BlogPost.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using AtlasBlog.Enums;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
@@ -8,6 +9,7 @@ public class BlogPost
 {
     public int Id { get; set; }
     
+    [Display(Name = "Blog Id")]
     public int BlogId { get; set; }
     
     [Required]
@@ -16,12 +18,14 @@ public class BlogPost
     
     public string Slug { get; set; }
     
+    [Display(Name = "Mark for Deletion?")]
     public bool IsDeleted { get; set; }
     
     [Required]
     [StringLength(200, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
     public string Abstract { get; set; } = "";
     
+    [Display(Name = "Post State")]
     public BlogPostState BlogPostState { get; set; }
     
     [Required]
