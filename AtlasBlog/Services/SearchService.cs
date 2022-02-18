c2fed833fa4fce8635e5ae37e23048e3811ba509
jedmorris <jemorris110@gmail.com>
2022-02-18 13:18:52 -0800

    completed search parameters for SearchService

--- AtlasBlog/Services/SearchService.cs
+++ AtlasBlog/Services/SearchService.cs
@@ -12,10 +12,27 @@ public class SearchService
         _dbContext = dbContext;
     }
 
-    public List<BlogPost> TermSearch(string searchTerm)
+    public IQueryable<BlogPost> TermSearch(string searchTerm)
     {
         var resultSet = _dbContext.BlogPosts
-            .Where(b => b.BlogPostState == Enums.BlogPostState.ProductionReady && !b.IsDeleted);
+            .Where(b => b.BlogPostState == Enums.BlogPostState.ProductionReady && !b.IsDeleted).AsQueryable();
+        
+        // If a search term is supplied, look for it in the resultSet
+        if (!string.IsNullOrEmpty(searchTerm))
+        {
+            resultSet = resultSet.Where(
+                b => b.Title.ToLower().Contains(searchTerm) ||
+                     b.Abstract.ToLower().Contains(searchTerm) ||
+                     b.Body.ToLower().Contains(searchTerm) ||
+                     b.Comments.Any(c =>
+                         c.CommentBody.ToLower().Contains(searchTerm) ||
+                         c.Author!.FirstName.ToLower().Contains(searchTerm) ||
+                         c.Author.LastName.ToLower().Contains(searchTerm) ||
+                         c.Author.Email.ToLower().Contains(searchTerm) ||
+                         c.ModerateBody!.ToLower().Contains(searchTerm)));
+        }
+
+        return resultSet.OrderByDescending(r => r.Created);
     }
 
 
