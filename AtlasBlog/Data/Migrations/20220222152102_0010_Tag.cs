d1f29db83b8d04859b8dce7c27bbca7947455647
jedmorris <jemorris110@gmail.com>
2022-02-22 07:22:06 -0800

    db update with Tag model and comment corrections

---
+++ AtlasBlog/Data/Migrations/20220222152102_0010_Tag.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace AtlasBlog.Data.Migrations
+{
+    public partial class _0010_Tag : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ModeratedBody",
+                table: "Comment",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModeratedDate",
+                table: "Comment",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ModerationReason",
+                table: "Comment",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModeratorId",
+                table: "Comment",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Text = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BlogPostTag",
+                columns: table => new
+                {
+                    BlogPostsId = table.Column<int>(type: "integer", nullable: false),
+                    TagsId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlogPostTag", x => new { x.BlogPostsId, x.TagsId });
+                    table.ForeignKey(
+                        name: "FK_BlogPostTag_BlogPosts_BlogPostsId",
+                        column: x => x.BlogPostsId,
+                        principalTable: "BlogPosts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BlogPostTag_Tags_TagsId",
+                        column: x => x.TagsId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_ModeratorId",
+                table: "Comment",
+                column: "ModeratorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BlogPostTag_TagsId",
+                table: "BlogPostTag",
+                column: "TagsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_AspNetUsers_ModeratorId",
+                table: "Comment",
+                column: "ModeratorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_AspNetUsers_ModeratorId",
+                table: "Comment");
+
+            migrationBuilder.DropTable(
+                name: "BlogPostTag");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comment_ModeratorId",
+                table: "Comment");
+
+            migrationBuilder.DropColumn(
+                name: "ModeratedBody",
+                table: "Comment");
+
+            migrationBuilder.DropColumn(
+                name: "ModeratedDate",
+                table: "Comment");
+
+            migrationBuilder.DropColumn(
+                name: "ModerationReason",
+                table: "Comment");
+
+            migrationBuilder.DropColumn(
+                name: "ModeratorId",
+                table: "Comment");
+        }
+    }
+}
