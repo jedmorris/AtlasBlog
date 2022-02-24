684f3d50b51802cabdb6b2df59dab8f1a73606e2
jedmorris <jemorris110@gmail.com>
2022-02-24 12:05:24 -0800

    updated HomeController to include pageNum functionality

--- AtlasBlog/Controllers/HomeController.cs
+++ AtlasBlog/Controllers/HomeController.cs
@@ -1,21 +1,32 @@
 ﻿using System.Diagnostics;
+using AtlasBlog.Data;
 using Microsoft.AspNetCore.Mvc;
 using AtlasBlog.Models;
+using X.PagedList;
 
 namespace AtlasBlog.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
+    public IActionResult Index(int? pageNum)
     {
-        return View();
+        pageNum ??= 1;
+        
+        // PagedLists always need to know what page to display and order them explicitly
+        var blogs = await _context.Blogs
+            .OrderByDescending(b => b.Created)
+            .ToPagedListAsync(pageNum, 5);
+        
+        return View(blogs);
     }
 
     public IActionResult Privacy()
